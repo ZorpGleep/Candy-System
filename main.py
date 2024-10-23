2c79723f68fdfeabd8f614c60b493f8f95e4934e
ZorpGleep <mrrocketleage+github@gmail.com>
2024-10-23 08:30:33 +0000

    Optimised code and made structure for csv table

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 shots = int(input("Shots 4 or 10:  "))
 shots_in = int(input("Amount oif shots in: "))
+a_price_no_margin = 13
+b_price_no_margin = 14
+c_price_no_margin = 6
+
+a_price = 15
+b_price = 20
+c_price = 10
+
+a_name = "Mini chupa chups"
+b_name = "Mentos mini chupa chups mix"
+c_name = "Sour straps"
 
 def prize_pick(amount):
     if amount < 6:
         return ["break"]
     print(f"{amount/100} left")
     def print_and_ask():
-        if amount > 12:
-            print("a: Mini chupa chups: 0.13")
-        if amount > 13:
-            print("b: Mentos mini chupa chups mix: 0.14")
-        if amount > 5:
-            print("c: Sour straps: 0.06")
+        if amount > a_price-1:
+            print(f"a: {a_name}: 0.13")
+        if amount > b_price-1:
+            print(f"b: {b_name}: 0.14")
+        if amount > c_price-1:
+            print(f"c: {c_name}: 0.06")
         x = str(input("enter option: "))
         return x
     x = print_and_ask()
     if x == "a":
-        if amount > 12:
+        if amount > a_price-1:
             return ["a", 13]
         else:
             print("not enough money!")
             return ["nanValue"]
     if x == "b":
-        if amount > 13:
+        if amount > b_price-1:
             return ["b", 14]
         else:
             print("not enough money!")
             return ["nanValue"]
     if x == "c":
-        if amount > 5:
+        if amount > c_price-1:
             return ["c", 6]
         else:
             print("not enough money!")
@@ -75,14 +86,16 @@ for lol in candy:
     if lol == "c":
         c += 1
         
-print(f"{a} x mini chupa chups")
-print(f"{b} x mentos mini chupa chups mix")
-print(f"{c} x SOur straps")
+print(f"{a} x {a_name}")
+print(f"{b} x {b_name}")
+print(f"{c} x {c_name}")
 
-with open("sales.txt", "a") as f:
-    if shots == 10:
-        paid = 2.5
-    else:
-        paid = 1
-    f.write(f"{(a*0.13) + (b * 0.14) + (c * 0.06)}|||{a} mini chupas, {b} mentos mini chupa chupas mix, {c} sour straps. Consumer paid ${paid}. Candy cost {(a*0.13) + (b * 0.14) + (c * 0.06)}. Total gain: {paid - ((a*0.13) + (b * 0.14) + (c * 0.06))}")
+# with open("sales.txt", "a") as f:
+#     if shots == 10:
+#         paid = 2.5
+#     else:
+#         paid = 1
+#     f.write(f"{(a*0.13) + (b * 0.14) + (c * 0.06)}|||{a} mini chupas, {b} mentos mini chupa chupas mix, {c} sour straps. Consumer paid ${paid}. Candy cost {(a*0.13) + (b * 0.14) + (c * 0.06)}. Total gain: {paid - ((a*0.13) + (b * 0.14) + (c * 0.06))}")
             
+with open("sales.txt", "a") as f:
+    f.write
